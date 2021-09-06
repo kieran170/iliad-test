9f32d2fafad74fb33bbd652dccf0cc0921277a1d
Kieran170 <kieran170@hotmail.co.uk>
2021-09-06 13:36:07 +0100

    adds button to submit a number, when number is submitted a piece of text will appear depending on what number you have entered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [int, setInt] = useState(null);
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    setInt(e.target.value)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (int % 7 === 0 && int % 5 === 0 && int % 3 === 0) {
+      setText('FizzBuzzBosh!');
+    } else if (int % 3 === 0 && int % 5 === 0) {
+      setText('FizzBuzz!');
+    } else if (int % 3 === 0 && int % 7 === 0) {
+      setText('FizzBosh!');
+    } else if (int % 5 === 0 && int % 7 === 0) {
+      setText('BuzzBosh!');
+    } else if (int % 3 === 0) {
+      setText('Fizz!');
+    } else if (int % 5 === 0) {
+      setText('Buzz!');
+    } else if (int % 7 === 0) {
+      setText('Bosh!');
+    }else {
+      setText('None!');
+    }
+  };
+
   return (
-    <div>
-      <p>Hey</p>
+    <div className='page-container'>
+      <div className='input-container'>
+        <form onSubmit={handleSubmit}>
+          <label style={{marginRight: '10px'}}>PLEASE ENTER A NUMBER</label>
+          <input min='0' value={int} onChange={handleChange} type='number'></input>
+          <button style={{cursor: 'pointer'}} type='submit'>Submit</button>
+        </form>
+        <div>
+          {text && <h1>{text}</h1>}
+        </div>
+      </div>
     </div>
   );
 }
